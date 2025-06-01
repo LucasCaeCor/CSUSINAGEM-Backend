e66c46a151af36952abdad53042da6dcdf774382
Lucas Cordeiro <Lucas_tb1315@hotmail.com>
2025-06-01 20:18:31 +0000

    attback

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,18 @@ import { routes } from './routes';
 
 const app = Fastify({ logger: true });
 
-// Configuração segura para CORS no Fastify
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://csusinagem-frontend.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://172.27.64.1:5173",
+    "https://csusinagem-frontend.vercel.app"
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 };
 
 const start = async () => {
-  // Registra o plugin CORS com as opções
   await app.register(cors, corsOptions);
 
   app.register(fastifyMultipart);
